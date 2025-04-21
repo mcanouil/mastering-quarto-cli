72253a612ff8d44bc5c2600a6897399b637b34af
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2025-04-21 10:23:42 +0200

    feat: use output file name for PDF decktape

--- assets/scripts/slides-to-pdf.sh
+++ assets/scripts/slides-to-pdf.sh
@@ -4,11 +4,19 @@ if [ -z "${QUARTO_PROJECT_RENDER_ALL}" ]; then
   exit 0
 fi
 
-SLIDES_FILES=$(echo "${QUARTO_PROJECT_OUTPUT_FILES}" | tr ' ' '\n' | grep -E 'slides\.html$')
+HTML_FILES=$(echo "${QUARTO_PROJECT_OUTPUT_FILES}" | tr ' ' '\n' | grep -E '\.html$')
+
+SLIDES_FILES=""
+for HTML_FILE in ${HTML_FILES}; do
+  if grep -q '<div class="reveal">' "${HTML_FILE}"; then
+    SLIDES_FILES="${SLIDES_FILES} ${HTML_FILE}"
+  fi
+done
+
+SLIDES_FILES=$(echo "${SLIDES_FILES}" | xargs)
 
 for SLIDES_PATH in ${SLIDES_FILES}; do
   echo "Processing ${SLIDES_PATH}"
-  SLIDES_DIR=$(dirname "${SLIDES_PATH}")
   
   PDF_AUTHOR=$(grep -o '<meta name="author" content="[^"]*"' "${SLIDES_PATH}" | sed 's/<meta name="author" content="\(.*\)"/\1/')
   PDF_TITLE=$(grep -o '<title>.*</title>' "${SLIDES_PATH}" | sed 's/<title>\(.*\)<\/title>/\1/')
@@ -20,5 +28,5 @@ for SLIDES_PATH in ${SLIDES_FILES}; do
     --load-pause 2000 \
     --pdf-author "${PDF_AUTHOR}" \
     --pdf-title "${PDF_TITLE}" \
-    "${SLIDES_PATH}" "${SLIDES_DIR}/slides.pdf"
+    "${SLIDES_PATH}" "${SLIDES_PATH%.html}.pdf"
 done
